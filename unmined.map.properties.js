5e2e624e1eb4a5183a7db726d5f36ab628b5eee5
Drew Pusey <55492673+drew654@users.noreply.github.com>
2023-06-11 22:17:41 -0500

    06/11/23 21:02:27

--- unmined.map.properties.js
+++ unmined.map.properties.js
@@ -8,7 +8,7 @@ var UnminedMapProperties = {
     minRegionZ: -3,
     maxRegionX: 4,
     maxRegionZ: 11,
-    worldName: "UNCHAINED 06/10/23 22:14:14",
+    worldName: "UNCHAINED 06/11/23 21:02:27",
     background: "#000000",
     markers: new Array()
 }
